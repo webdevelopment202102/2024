ebed5b450e8788e18ff97051eee663f893fcd396
5-PGphotographer(editing) <abdallahmahrous499@gmail.com>
2021-09-02 02:43:36 +0300

    initial commit

---
+++ template-1/java.js
@@ -0,0 +1,17 @@
+
+/*glopal console, alert, prompt*/
+function nono() {
+    "use strict";
+    var x = prompt("what is your name");
+    if (x === "abdallah") {
+        alert("good");
+    }
+} ;
+console.log(nono());
+function nono() {
+    "use strict";
+    var x = prompt("what is your name");
+    for (x = 0; x >= 10; x++) ;
+    
+} ;
+console.log(nono());
